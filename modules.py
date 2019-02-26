1359aa037a96fcf44c56fc0f83d7f152b5043b25
aaronBurns59 <aaronburns59@gmail.com>
2019-02-26 20:52:41 +0000

    using math, csv and datetime modules

---
+++ modules.py
@@ -0,0 +1,24 @@
+import datetime as dt
+import csv
+import math
+
+
+
+#datetime.now()
+print("datetime(dt).now")
+print(dt.datetime.now())
+
+#math.floor
+print("math.floor")
+print(math.floor(1.23))
+#math.factorial
+print("math.factorial")
+print(math.factorial(5))
+#math.gcd(greatest common diviser)
+print("math.gcd")
+print(math.gcd(10, 65))
+
+
+
+
+dt.datetime.strftime(dt.datetime.now(), '%Y-%n-%d-%H-%M-%S.csv')
